21dba33965f14ddca12ff23fa1b71b310e968020
Yardley Lagombra <ylagombra@yahoo.com>
2016-04-08 18:13:12 -0400

    Initial commit

--- multiples/ViewController.swift
+++ multiples/ViewController.swift
@@ -10,15 +10,73 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet weak var logo: UIImageView!
+    @IBOutlet weak var addingLabel: UILabel!
+    @IBOutlet weak var selectLabel: UITextField!
+    @IBOutlet weak var addButton: UIButton!
+    @IBOutlet weak var playButton: UIButton!
+    @IBOutlet weak var background: UIImageView!
+    @IBOutlet weak var endButton: UIButton!
+    
+    var counter = 0
+    var multiple = 0
+    
+    // function to present game to screen 
+    @IBAction func playButtonClicked(sender: UIButton) {
+        
+        
+    if selectLabel.text != nil && selectLabel.text != ""
+        {
+        
+        logo.hidden = true
+        selectLabel.hidden = true
+        playButton.hidden = true
+        
+        addingLabel.hidden = false
+        addButton.hidden = false
+        endButton.hidden = false
+        
+        }
+      else
+        {
+        
+        }
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    //Adds textbox number and outputs to label
+    @IBAction func clickToAdd(sender: UIButton) {
+        
+         multiple = Int(selectLabel.text!)!
+        
+        
+        addingLabel.text = "\(counter) + \(multiple) = \(counter + multiple) "
+        
+        counter = counter + multiple
     }
+    
+    //Function to terminate game and reset
+    @IBAction func endGame(sender: UIButton) {
+        
+        endButton.hidden = true
+        addButton.hidden = true
+        addingLabel.hidden = true
+        
+        logo.hidden = false
+        selectLabel.hidden = false
+        playButton.hidden = false
+        
+        counter = 0
+        multiple = 0
+        
+        addingLabel.text = "Press Play to add!"
+        
+        
+    }
+    
+    
+    
+    
 
 
 }
